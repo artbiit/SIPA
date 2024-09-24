9b8ff8719e68a49c8bfbe7c3e1283fa147816439
ljs <artbiit@naver.com>
2024-09-24 16:56:09 +0900

    상점 관련 병합 작업

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,18 +1,15 @@
 import Utils from '../lib/utils.js';
 import { authenticateToken } from '../middleware/auth-middleware.js';
 import { tokenVerify } from '../middleware/token-middleware.js';
-<<<<<<< HEAD
 import gameRoutes from './game-route.js';
 import userRoutes from './users-route.js';
-=======
 import purchaseRoute from '../routes/purchase-route.js';
 
->>>>>>> origin/feature/cash_purchase
 const allRoutes = [
   ...gameRoutes,
-  ...userRoutes
-  // 다른 라우트 추가 가능
+  ...userRoutes,
   ...purchaseRoute,
+  // 다른 라우트 추가 가능
 ];
 
 // 파라미터 및 미들웨어 자동 설정
